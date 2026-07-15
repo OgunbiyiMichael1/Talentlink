70741ff1c13b6717aa6f3a890b4b48fed33ade42
OgunbiyiMichael1 <mogunbiyi2010@gmail.com>
2026-07-15 12:36:07 +0100

    add vercel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,15 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5501', 'http://127.0.0.1:5501', 'https://talentlink-giqi.onrender.com'],
+  origin: [
+    'http://localhost:5500', 
+    'http://127.0.0.1:5500', 
+    'http://localhost:5501', 
+    'http://127.0.0.1:5501',
+    'https://talentlink-topaz.vercel.app'
+  ],
   credentials: true
 }))
-
 
 //testing
 app.get("/", (req,res) => {
